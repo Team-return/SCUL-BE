2f86e9312c8109a4896bb16091dd333f5d36f45e
tae.y <0211ilyoil@gmail.com>
2024-04-25 15:19:37 +0900

    🦷 :: 쓸데없는거 정리

--- src/main/kotlin/scul/projectscul/domain/culture/service/GetCultureListService.kt
+++ src/main/kotlin/scul/projectscul/domain/culture/service/GetCultureListService.kt
@@ -2,14 +2,12 @@ package scul.projectscul.domain.culture.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import scul.projectscul.domain.bookmark.domain.BookMark
 import scul.projectscul.domain.bookmark.domain.repository.BookMarkRepository
 import scul.projectscul.domain.culture.domain.Culture
 import scul.projectscul.domain.culture.domain.repository.CultureRepository
 import scul.projectscul.domain.culture.presentation.dto.response.GetCultureListResponse
 import scul.projectscul.domain.user.domain.User
 import scul.projectscul.domain.user.facade.UserFacade
-import java.util.UUID
 
 
 @Service
--- src/main/kotlin/scul/projectscul/domain/review/service/GetMyReviewService.kt
+++ src/main/kotlin/scul/projectscul/domain/review/service/GetMyReviewService.kt
@@ -2,7 +2,6 @@ package scul.projectscul.domain.review.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import scul.projectscul.domain.culture.domain.repository.CultureRepository
 import scul.projectscul.domain.review.domain.repository.ReviewRepository
 import scul.projectscul.domain.review.presentation.dto.response.GetReviewsResponse
 import scul.projectscul.domain.user.facade.UserFacade
@@ -11,7 +10,6 @@ import scul.projectscul.domain.user.facade.UserFacade
 @Transactional(readOnly = true)
 class GetMyReviewService (
         private val reviewRepository: ReviewRepository,
-        private val cultureRepository: CultureRepository,
         private val userFacade: UserFacade
 ) {
 
